783962aac8b7ad07daf2ea1cdeda0320441f292d
Jonathan Lagneaux <lagneaux.j@gmail.com>
2021-10-04 00:16:50 +0200

    fix ios app
    fix #4

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val compose = "1.0.3"
-    const val coroutine = "1.5.0"
+    const val coroutine = "1.5.0-native-mt"
     const val ktor = "1.6.4"
     const val material = "1.3.0"
     const val kotlinxSerialization = "1.0.1"
